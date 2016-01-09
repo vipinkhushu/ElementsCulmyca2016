72e351515c3bcc26811eda5b2152cab63cec3382
Ask Prateek <Ask Prateek>
2016-01-09 16:35:58 +0530

    Static flles link

---
+++ header.php
@@ -0,0 +1,12 @@
+<?php include 'include/site-settings.php'; ?>
+<head>
+  <title><?php echo $site_name; ?></title>
+  <!--Import Google Icon Font-->
+  <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css | Add links to other css files after next line -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+  <link rel="stylesheet" href="<?php echo $css_url; ?>">
+
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
