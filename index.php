dd62179796000580355fcb775424584b8d0cdfa1
Ask Prateek <Ask Prateek>
2016-01-09 16:38:02 +0530

    Basic Index structure

---
+++ index.php
@@ -0,0 +1,30 @@
+<html>
+<?php include 'header.php'; ?>        <!-- Static Files -->
+<body>
+  <header>
+    <nav>                             <!-- Navigation Bar -->
+      <div class="nav-wrapper">
+        <a href ="/" class="brand-logo left thin">Rias Gremory</a>
+        <ul id="nav-mobile" class="right">
+
+        </ul>
+      </div>
+    </nav>                            <!-- Navigation bar ends -->
+  </header>
+
+  <main>
+
+    <div class="container">           <!-- Main Body Content -->
+    </div>                            <!-- Body Content ends -->
+
+  </main>
+
+  <?php include 'footer.php' ?>
+
+  <script>
+  $(document).ready(function(){
+    console.log('test');
+  });
+  </script>
+</body>
+</html>
