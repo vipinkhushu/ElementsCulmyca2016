e3f2d73160f5882c2302dd6a9b7220566e737ba8
Ask Prateek <Ask Prateek>
2016-01-09 16:36:29 +0530

    Dynamic files and Footer

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer class="page-footer">
+  <div class="row footer-copyright">
+    <div class="col s12 center-align">
+      <p>Elements Culmyca 2016</p>
+    </div>
+  </div>
+</footer>
+<!--Import jQuery before materialize.js-->
+<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+<script src="public/js/script.js"></script>
